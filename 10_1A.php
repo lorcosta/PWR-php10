dfca2a6f37ee692e9affbd96d1cca9ffb8efd499
Lorenzo <costa.lorenzo98@gmail.com>
2020-05-29 09:50:03 +0200

    Primo esercizio

---
+++ 10_1A.php
@@ -0,0 +1,18 @@
+<?php
+setcookie("Country","IT");
+ ?>
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <title>PHP 10_1A</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Lorenzo Costa" >
+    <link rel=stylesheet href=fogliodistile.css>
+  </head>
+  <body>
+      <h1>Pagina realizzata con PHP</h1>
+      <p>Autore della pagina: Lorenzo Costa</p>
+      <p>Questa pagina imposta un cookie con nome "Country" e rimanda ad una seconda pagina che lo legge</p>
+      <a href="10_1B.php">Clicca qui per la pagina di lettura del cookie</a>
+  </body>
+</html>
